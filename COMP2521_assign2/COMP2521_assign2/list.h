1bc665904259165455de4ed3973dde56167794e4
83n38 <83nhendo@gmail.com>
2018-10-09 01:03:05 +1100

    commit the Xcode file

---
+++ COMP2521_assign2/COMP2521_assign2/list.h
@@ -0,0 +1,22 @@
+// Linked list interface ... COMP2521
+
+#ifndef list_h
+#define list_h
+
+#include <stdbool.h>
+
+typedef struct Node *List;
+
+typedef struct Node {
+    char*         url;
+    struct Node *next;
+} Node;
+
+List insertLL(List, char *url);
+List deleteLL(List, char *url);
+bool inLL(List, char *url);
+void freeLL(List);
+void showLL(List);
+int lengthLL(List);
+
+#endif
